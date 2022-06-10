43411d96568bc75e2487abaaead190725f7b3832
Wayne <99174654+ugly-saifu@users.noreply.github.com>
2022-06-10 20:30:59 +0530

    Update and rename evolution_X01AD.mk to lineage_X01AD.mk

--- lineage_X01AD.mk
+++ lineage_X01AD.mk
@@ -22,13 +22,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/asus/X01AD/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Device identifiers.
-PRODUCT_NAME := evolution_X01AD
+PRODUCT_NAME := lineage_X01AD
 PRODUCT_DEVICE := X01AD
 PRODUCT_MANUFACTURER := asus
 PRODUCT_BRAND := asus
@@ -36,10 +36,7 @@ PRODUCT_MODEL := ASUS Max M2
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
-evolution_BUILD_MAINTAINER := Wayne
-
 # ArrowOS additions
-Evolution_BUILD_MAINTAINER := Wayne
 
 DEVICE_MAINTAINER := Wayne
 
